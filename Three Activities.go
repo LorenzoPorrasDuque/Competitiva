640c09700d017ca90f4ca8bfb4b20d7bbb24f508
Lorenzo Porras Duque <lorenzo.porras.duque@gmail.com>
2024-02-28 08:25:15 -0500

    coisas

--- Three Activities.go
+++ Three Activities.go
@@ -27,7 +27,29 @@ func createMatrix(n int) [][]int {
 	return matrix
 }
 func findMaxFriends(matrix [][]int) int {
-	var max, ubi, sum int
+	var sum int
 
+	for i1, ints1 := range matrix[0] {
+		for i2, ints2 := range matrix[1] {
+			for i3, ints3 := range matrix[2] {
+				if i2 != i1 && i3 != i2 && ints1+ints2+ints3 > sum {
+					sum = ints1 + ints2 + ints3
+				}
+			}
+		}
+	}
 	return sum
 }
+
+/*
+func findMaxFriends(matrix [][]int) int {
+	var sum int
+	for i := 0; i < len(matrix[0]); i++ {
+		for j := 0; j < len(matrix[1]); j++ {
+			for k := 0; k < len(matrix[2]); k++ {
+
+			}
+		}
+	}
+
+*/
